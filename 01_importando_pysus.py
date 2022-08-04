e667cd7bddae012fa515717ae63366642b50bcba
daniellybx <danielly.xavier@outlook.com>
2022-08-03 21:03:25 -0300

    atualizando notebook

--- 01_importando_pysus.py
+++ 01_importando_pysus.py
@@ -5,10 +5,10 @@ from ftplib import FTP
 from pysus.online_data.SIH import download
 
 #DEFININDO VARIÁVEIS
-vars = ["MORTE", "CID_MORTE", "DIAG_PRINC", "DI_INTER", "ANO_CMPT", "MES_CMPT", "DIAG_SECUN",  "IDADE", "SEXO", "UF_ZI", "DIAS_PERM", "NUM_FILHOS", "INSTRU", "MARCA_UTI", "INFEHOSP"]
+vars = ["MORTE", "CID_MORTE", "DIAG_PRINC", "DT_INTER", "ANO_CMPT", "MES_CMPT", "DIAG_SECUN",  "IDADE", "SEXO", "UF_ZI", "DIAS_PERM", "NUM_FILHOS", "INSTRU", "MARCA_UTI", "INFEHOSP"]
 ufs = ['ac', 'al', 'ap', 'am','ba', 'ce', 'df', 'es', 'go', 'ma', 'mt', 'ms', 'mg', 'pa', 'pb', 'pr', 'pe', 'pi', 'rj', 'rn', 'rs', 'ro', 'rr', 'sc', 'sp', 'se', 'to']
-anos = [2022] #[2017, 2018, 2019, 2020, 2021]
-meses = [1, 2, 3, 4] #, 5, 6, 7, 8, 9, 10, 11, 12]
+anos = [2022] #[2017, 2018, 2019, 2020, 2021] #
+meses = [1, 2, 3, 4] #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] #
 cids = ["J130", "J140", "J153", "J154", "J158", "J159", "J181"]
 
 #IMPORTANDO DADOS
@@ -20,4 +20,3 @@ for uf in ufs:
             df = df[df['DIAG_PRINC'].isin(cids)]
             df.to_csv("sih_pneumonia_1721.csv", mode='a', index=False, header=False)
             print(f"O arquivo do mês {mes} de {ano} do estado {uf.upper()} foi filtrado")
-

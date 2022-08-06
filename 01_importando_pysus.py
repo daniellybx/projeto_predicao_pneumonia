ce78f12eb9460ead3e17bf49271b9e85726e9bcb
daniellybx <danielly.xavier@outlook.com>
2022-08-06 17:47:14 -0300

    incluindo análise de frequencia das variáveis

--- 01_importando_pysus.py
+++ 01_importando_pysus.py
@@ -7,16 +7,16 @@ from pysus.online_data.SIH import download
 #DEFININDO VARIÁVEIS
 vars = ["MORTE", "CID_MORTE", "DIAG_PRINC", "DT_INTER", "ANO_CMPT", "MES_CMPT", "DIAG_SECUN",  "IDADE", "SEXO", "UF_ZI", "DIAS_PERM", "NUM_FILHOS", "INSTRU", "MARCA_UTI", "INFEHOSP"]
 ufs = ['ac', 'al', 'ap', 'am','ba', 'ce', 'df', 'es', 'go', 'ma', 'mt', 'ms', 'mg', 'pa', 'pb', 'pr', 'pe', 'pi', 'rj', 'rn', 'rs', 'ro', 'rr', 'sc', 'sp', 'se', 'to']
-anos = [2017, 2018, 2019, 2020, 2021, 2022] 
+anos = [2017, 2018, 2019, 2020, 2021] #2022 
 meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] 
-cids = ["J130", "J140", "J153", "J154", "J158", "J159", "J181"]
+cids = ["J13", "J14", "J153", "J154", "J158", "J159", "J181"]
 
 #IMPORTANDO DADOS
-for ano in anos:
-    for uf in ufs:        
-        for mes in meses:
+for mes in meses: 
+    for uf in ufs:
+        for ano in anos:   
             df = download(uf, ano, mes)
             df = df.filter(vars)
             df = df[df['DIAG_PRINC'].isin(cids)]
             df.to_csv("sih_pneumonia_1721.csv", mode='a', index=False, header=False)
-            print(f"O arquivo do mês {mes} de {ano} do estado {uf.upper()} foi filtrado")
+            print(f"O arquivo do mês {mes} de {'2022'} do estado {uf.upper()} foi filtrado")
